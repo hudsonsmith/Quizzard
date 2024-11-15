0ef1af7b04fdf4e4f315eb966c3e746ffe7b3bd2
Hudson Smith <76886104+hudsonsmith@users.noreply.github.com>
2024-11-14 21:42:49 -0600

    Saving progress

---
+++ src/assets/js/script.js
@@ -0,0 +1,19 @@
+// Just for testing.
+// TODO: REMOVE
+localStorage.clear();
+
+if (! localStorage.getItem("quiz-data")) {
+    localStorage.setItem("quiz-data", {})
+}
+
+const newQuizForm = document.getElementById("newQuizForm");
+
+newQuizForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    let name = newQuizForm.elements["name"].value;
+    let quizData = JSON.parse(localStorage.getItem("quiz-data"));
+
+    quizData[name] = {"questions": ["hey"]};
+    localStorage.setItem("quiz-data", JSON.stringify(quizData));
+});
